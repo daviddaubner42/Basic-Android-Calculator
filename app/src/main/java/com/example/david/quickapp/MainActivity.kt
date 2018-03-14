cac2154178ec48ea8314541c32f4c3475c6cb995
davisdaubner <davidaubner@gmail.com>
2018-03-14 20:54:58 +0100

    Name change

--- app/src/main/java/com/example/david/quickapp/MainActivity.kt
+++ app/src/main/java/com/example/david/quickapp/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "The Superb Calculator"
+        title = "The Basic Calculator"
 
         val plusButton = findViewById<Button>(R.id.plus)
         plusButton.setOnClickListener { setOp('+') }
